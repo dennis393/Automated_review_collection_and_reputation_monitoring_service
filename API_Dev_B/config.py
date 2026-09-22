b8705311ae0da0c1c50bb9e2348d4b5833983e99
dennis393 <jorikms30@gamil.com>
2026-09-22 13:10:58 +0500

    add openrouter_token in model

--- API_Dev_B/config.py
+++ API_Dev_B/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     LIVE_MINUTES_TOKEN: int = 30
     ENCRYPTION_KEY: str
     TG_BOT_TOKEN: str
+    OPENROUTER_TOKEN: str
     
     model_config = SettingsConfigDict(env_file=Path(__file__).parent.parent / ".env", env_file_encoding="utf-8", extra="ignore") #Если файлы находятся в на разных уровнях юзаем это
 
